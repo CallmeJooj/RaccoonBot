226273287dd65e588eb9b056ce173161b796d131
jordano.silva <jordano.silva@cwi.com.br>
2022-03-08 17:35:12 -0300

    resolvido

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import Discord from 'discord.js'
+import Discord, { MessageFlags } from 'discord.js'
 import axios from 'axios'
 const client = new Discord.Client();
 
@@ -25,26 +25,29 @@ client.on('message', async (msg) => {
     if (["!r coon", "!raccoon"].includes(msg.content)) {
         const reply = (await myAPI('coon')).image
         msg.channel.send(msg.author, {files: [reply]})
+        msg.react('🦝')
     } 
     else 
     if (["!r coon bomb", "!raccoon bomb"].includes(msg.content)) {
         const reply = (await myAPI('coons')).map(reply => reply.image)
-        msg.channel.send(msg.author) 
-        reply.array.forEach(reply => {
-            msg.channel.send({files: [reply]})
+        msg.channel.send(`${msg.author}`) 
+        reply.forEach(element => {
+            msg.channel.send(element)
         });
+        msg.react('🦝')
     }
     else 
     if(msg.content === "!r fact"){
         const reply = (await raccoonApi()).fact
         msg.channel.send(reply)
+        msg.react('🦝')
     }
     else 
     if(["!r", "!r help", "!raccoon help"].includes(msg.content)){
         const reply = `\`\`\`!r coon/!r raccoon - Gives you a random raccoon picture\`\`\`\`\`\`!r fact - Gives you a random raccoon fact\`\`\`` 
         msg.channel.send(reply)
+        msg.react('🦝')
     }
-    msg.react('🦝')
 });
 
 client.login('OTQ3MTg0NTk0NjQ0NTEyODM4.YhpkZQ.Qx0OaZxdp2jD5OS00zu4XFShDYE')
